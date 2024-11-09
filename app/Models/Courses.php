e4234797ff42a05e75c227d7c77024dd0206aa6a
Rio Ragil Ramdani <rioragilramdani7@gamail.com>
2024-11-09 18:08:31 +0700

    Add relationships to Courses model for Categories and Teacher

--- app/Models/Courses.php
+++ app/Models/Courses.php
@@ -2,11 +2,21 @@
 
 namespace App\Models;
 
+use App\Models\Categories;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Courses extends Model
 {
     protected $table = 'courses';
     protected $fillable = ['name', 'slug', 'path_trailer', 'about', 'thumbnail', 'category_id', 'teacher_id'];
     public $timestamps = true;
+    public function categoriesconn(): BelongsTo
+    {
+        return $this->belongsTo(Categories::class, 'category_id');
+    }
+    public function teacherconn()
+    {
+        return $this->belongsTo(Teacher::class, 'teacher_id');
+    }
 }
