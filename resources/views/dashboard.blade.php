14d7ef8d98569baae5cba242ffe6ed3ae9fa6ee3
andremilano <andremilano16@gmail.com>
2024-11-11 10:32:01 +0700

    Refactor category edit and update methods: use category ID for fetching, improve slug handling, and manage icon file deletion

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -19,9 +19,12 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <h1>Parent Section</h1>
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <h1>Outer Section</h1>
                 <div class="p-6 text-gray-900">
                     {{ __("You're logged in!") }}
+                    <h1>Inner Section</h1>
                 </div>
             </div>
         </div>
