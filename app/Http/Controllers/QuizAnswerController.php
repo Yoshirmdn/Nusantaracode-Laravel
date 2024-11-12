720390e99b665f2cca3637d62bb69cb2c6d04a4f
Rio Ragil Ramdani <rioragilramdani7@gamail.com>
2024-11-12 11:46:13 +0700

    Add Quiz and QuizAnswer resources with migrations and controllers

---
+++ app/Http/Controllers/QuizAnswerController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Quiz_answer;
+use Illuminate\Http\Request;
+
+class QuizAnswerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Quiz_answer $quiz_answer)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Quiz_answer $quiz_answer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Quiz_answer $quiz_answer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Quiz_answer $quiz_answer)
+    {
+        //
+    }
+}
