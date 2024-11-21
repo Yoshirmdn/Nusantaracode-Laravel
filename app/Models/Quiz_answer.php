cc6cc4b2fe1d98fe509c298574af29f073d0a252
Rio Ragil Ramdani <rioragilramdani7@gamail.com>
2024-11-21 21:14:30 +0700

    Define Quiz_answer model properties and relationships

--- app/Models/Quiz_answer.php
+++ app/Models/Quiz_answer.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Quiz_answer extends Model
 {
-    //
+    protected $table = 'quiz_answers';
+    protected $fillable = ['quiz_id', 'answer', 'is_correct'];
+    public $timestamps = true;
+    public function quiz()
+    {
+        return $this->belongsTo(Quiz::class, 'quiz_id');
+    }
 }
