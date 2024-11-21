f3abeff0b0fafaab5fc213192e861e9fd23c5ab7
Rio Ragil Ramdani <rioragilramdani7@gamail.com>
2024-11-21 21:14:34 +0700

    Define Quiz model properties and relationships

--- app/Models/Quiz.php
+++ app/Models/Quiz.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Quiz extends Model
 {
-    //
+    protected $table = 'quizzes';
+    protected $fillable = ['lesson_id', 'question'];
+    public $timestamps = true;
+    public function lesson()
+    {
+        return $this->belongsTo(Lesson::class, 'lesson_id');
+    }
 }
