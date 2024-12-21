625a02f827f85159ed7189d4377ba2f55e8d57be
Rio Ragil Ramdani <rioragilramdani7@gamail.com>
2024-12-21 18:55:04 +0700

    Enhance LessonStudentController to include lessons and keypoints, and add 404 error handling for missing courses

--- app/Http/Controllers/LessonStudentController.php
+++ app/Http/Controllers/LessonStudentController.php
@@ -9,9 +9,19 @@ class LessonStudentController extends Controller
 {
     public function index($id)
     {
-        $course = Courses::with(['categoriesconn', 'teacherconn.user'])
-            ->withCount('studentCourse')
+        $lessonStudent = Courses::with([
+            'categoriesconn',  // Kategori kursus
+            'teacherconn.user', // Pengajar dan data user terkait
+            'lessons',         // Pelajaran kursus
+            'keypoints',       // Poin penting kursus
+            'studentCourse'    // Siswa yang terdaftar
+        ])->withCount('studentCourse') // Menghitung jumlah siswa yang terdaftar
             ->find($id);
-        return view('user.courselayout', compact('course'));
+
+        if (!$lessonStudent) {
+            abort(404, 'Course not found.');
+        }
+
+        return view('user.courselayout', compact('lessonStudent'));
     }
 }
