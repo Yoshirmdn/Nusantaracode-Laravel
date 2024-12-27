f49452b775858ccd4558fa959d28cb11b9423dc3
Rio Ragil Ramdani <rioragilramdani7@gamail.com>
2024-12-27 19:12:37 +0700

    Add Midtrans payment gateway configuration to services

--- config/services.php
+++ config/services.php
@@ -13,6 +13,12 @@ return [
     | a conventional file to locate the various service credentials.
     |
     */
+    'midtrans' => [
+        'merchant_id' => env('MIDTRANS_MERCHANT_ID', ''),
+        'client_key' => env('MIDTRANS_CLIENT_KEY', ''),
+        'server_key' => env('MIDTRANS_SERVER_KEY', ''),
+        'is_production' => env('MIDTRANS_IS_PRODUCTION', false),
+    ],
 
     'postmark' => [
         'token' => env('POSTMARK_TOKEN'),
